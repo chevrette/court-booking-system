63c248e935063a2db4e9a756f47b55de729ca195
chevrette <denkiewicz.karolina@gmail.com>
2019-05-21 00:32:21 +0200

    Add basic endpoints for courts

--- server/router.js
+++ server/router.js
@@ -1,7 +1,71 @@
+const NHOURS = 24;
+const NCOURTS = 6;
+const START_HOUR = 6;
+const END_HOUR = 21;
+
+class Reservation {
+  constructor(from, to, owner) {
+    this.from = from;
+    this.to = to;
+    this.owner = owner;
+  }
+
+  serialize() {
+    return {
+      from: this.from,
+      to: this.to,
+      owner: this.owner
+    };
+  }
+}
+
+class Court {
+  constructor(id) {
+    this.id = id;
+    this.reservations = [];
+  }
+
+  serialize() {
+    return {
+      id: this.id,
+      reservations: this.reservations.map(r => r.serialize())
+    };
+  }
+
+  makeReservation({ from, to, owner }) {
+    if (!this.isAvailable(from, to)) {
+      throw new Error("Already taken");
+    }
+
+    this.reservations.push(new Reservation(from, to, owner));
+  }
+
+  isAvailable(from, to) {
+    // TODO
+    return this.reservations.filter(_ => false) == 0;
+  }
+}
+
+const courts = [...Array(6).keys()].map(id => new Court(id));
+
 module.exports = {
-  initRoutes: (app) => {
-    app.get('/api/hello', (req, res) => {
-      res.json({ message: 'Hello from backend' });
-    });
+  initRoutes: app => {
+    app
+      .get("/api/courts", (req, res) => {
+        res.json(courts.map(court => court.serialize()));
+      })
+      .get("/api/courts/:id", function(req, res) {
+        res.json(courts[req.params.id].serialize());
+      })
+      .post("/api/courts/:id/reservations", function(req, res) {
+        const court = courts[req.params.id];
+        try {
+          court.makeReservation(req.body);
+        } catch (err) {
+          res.status(400);
+          res.json({ message: err });
+        }
+        res.json(courts[req.params.id].serialize());
+      });
   }
 };
--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
-const express = require('express'),
-        bodyParser = require('body-parser'),
-        cookieParser = require('cookie-parser'),
-        router = require('./router');
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  cookieParser = require("cookie-parser"),
+  router = require("./router");
 
 const app = express();
+const port = 4000;
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(bodyParser.json());
 app.use(cookieParser());
 router.initRoutes(app);
 
-const server = app.listen(4000, () => {
-  console.log('Server started');
+const server = app.listen(port, () => {
+  console.log("Server started");
 });
-
